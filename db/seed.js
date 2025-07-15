fea26232d8d675feaf2ee5ef1ef88b1452e79ba2
bric12369 <barryrichards05@gmail.com>
2025-07-15 20:42:06 +0100

    created first table

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,14 @@
-
+const db = require('./connection')
 
 async function seed() {
+    await db.query(`DROP TABLE IF EXISTS property_types`)
+
+    await db.query(`CREATE TABLE property_types(
+        property_type VARCHAR NOT NULL,
+        description TEXT NOT NULL
+        )`)
     
+    console.log('property_types created')
 }
 
 module.exports = seed
