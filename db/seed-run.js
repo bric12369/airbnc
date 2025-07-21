fada35a8ecf4afa5e5b1e19d73f74182b4928d8f
bric12369 <barryrichards05@gmail.com>
2025-07-21 18:32:24 +0100

    data insertion now in separate file

--- db/seed-run.js
+++ db/seed-run.js
@@ -1,4 +1,3 @@
 const seed = require('./seed')
-const { formattedPropertyTypesData, formattedUsersData, finalFormattedProperties, finalFormattedReviews, finalFormattedImages, finalFormattedFavourites, finalFormattedBookings, uniqueAmenities, formattedPropertiesAmenitiesData } = require('./format-all-raw-data')
 
-seed(formattedPropertyTypesData, formattedUsersData, finalFormattedProperties, finalFormattedReviews, finalFormattedImages, finalFormattedFavourites, finalFormattedBookings, uniqueAmenities, formattedPropertiesAmenitiesData)
+seed()
--- db/seed.js
+++ db/seed.js
@@ -1,75 +1,14 @@
-const db = require('./connection')
-const pgFormat = require('pg-format')
 const dropAllTables = require('./drop-all-tables')
 const createAllTables = require('./create-all-tables')
+const insertAllData = require('./insert-all-data')
 
-async function seed(formattedPropertyTypesData, formattedUsersData, finalFormattedProperties, finalFormattedReviews, finalFormattedImages, finalFormattedFavourites, finalFormattedBookings, uniqueAmenities, formattedPropertiesAmenitiesData) {
+async function seed() {
+    
     await dropAllTables()
 
     await createAllTables()
 
-    await db.query(
-        pgFormat(
-            `INSERT INTO property_types (property_type, description) VALUES %L`,
-            formattedPropertyTypesData
-        )
-    )
-
-    await db.query(
-        pgFormat(
-            `INSERT INTO users (first_name, surname, email, phone_number, is_host, avatar) VALUES %L`,
-            formattedUsersData
-        )
-    )
-
-    await db.query(
-        pgFormat(
-            `INSERT INTO properties (host_id, name, location, property_type, price_per_night, description) VALUES %L`,
-            finalFormattedProperties
-        )
-    )
-
-    await db.query(
-        pgFormat(
-            `INSERT INTO reviews (property_id, guest_id, rating, comment, created_at) VALUES %L`,
-            finalFormattedReviews
-        )
-    )
-
-    await db.query(
-        pgFormat(
-            `INSERT INTO images (property_id, image_url, alt_text) VALUES %L`,
-            finalFormattedImages
-        )
-    )
-    
-    await db.query(
-        pgFormat(
-            `INSERT INTO favourites (guest_id, property_id) VALUES %L`,
-            finalFormattedFavourites
-        )
-    )
-
-    await db.query(
-        pgFormat(
-            `INSERT INTO bookings (property_id, guest_id, check_in_date, check_out_date) VALUES %L`,
-            finalFormattedBookings
-        )
-    )
-
-    await db.query(
-        pgFormat(
-            `INSERT INTO amenities (amenity) VALUES %L`,
-            uniqueAmenities
-        )
-    )
-    
-    await db.query(
-        pgFormat(
-            `INSERT INTO properties_amenities (property_id, amenity_slug) VALUES %L`,
-            formattedPropertiesAmenitiesData
-        )
-    )
+    await insertAllData()
 }
 
 module.exports = seed
