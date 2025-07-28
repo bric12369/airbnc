f1b05db6ec4efd90f223163fbf382cc6a7c8d851
bric12369 <barryrichards05@gmail.com>
2025-07-28 17:28:23 +0100

    edited db query to order by favourites

--- db/models/properties.model.js
+++ db/models/properties.model.js
@@ -2,13 +2,16 @@ const db = require('../connection')
 
 const fetchAllProperties = async () => {
     const { rows } = await db.query(
-        `SELECT property_id,
+        `SELECT properties.property_id,
         name AS property_name,
         location,
         price_per_night,
         CONCAT(first_name, ' ', surname) AS host
         FROM properties
-        JOIN users on properties.host_id = users.user_id;`
+        JOIN users ON properties.host_id = users.user_id
+        LEFT JOIN favourites ON properties.property_id = favourites.property_id
+        GROUP BY properties.property_id, name, location, price_per_night, users.first_name, users.surname
+        ORDER BY COUNT (favourites.favourite_id) DESC;`
     )
     return rows
 }
